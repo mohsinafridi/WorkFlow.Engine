6d1647332fe9daad4cdeb8e516e8ebc78d4bb40b
Mohsin Azam <mohsin.azam@systemsltd.com>
2021-01-29 18:58:45 +0500

    project names updated

---
+++ Infrastructure.Persistence/Interfaces/IWorkFlow.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Repository.Interfaces
+{
+    public interface IWorkFlow
+    {
+        object StartWorkflow();
+        object CheckWorkflowStatus();
+    }
+}
---
+++ Workflow.Engine.Scheduler/Job.cs
@@ -0,0 +1,21 @@
+﻿using Quartz;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Scheduler
+{
+    public class Job : IJob
+    {
+        public Task Execute(IJobExecutionContext context)
+        {
+            //Sample code that denotes the job to be performed
+            using (StreamWriter streamWriter = new StreamWriter(@"D:\IDGLog.txt", true))
+            {
+                streamWriter.WriteLine(DateTime.Now.ToString());
+            }
+        }
+    }
+}
